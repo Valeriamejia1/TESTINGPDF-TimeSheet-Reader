45acb60b58b7b099716aaaa0615797097cc570f9
RodrigoFallas <RodrigoFallas@CR-RFALLAS>
2023-08-18 16:58:49 -0600

    Convert

--- convert_pdf.py
+++ convert_pdf.py
@@ -1,4 +1,5 @@
 from Modules.pdfPaylocity import main
+import os
 
 # Lista de nombres de archivos PDF a convertir
 pdf_files = ["PDF/Haynie, Nathan.pdf", "PDF/Hobbs, Brandy.pdf"]
@@ -7,9 +8,16 @@ reportType = "Paylocity"
 for pdf_file in pdf_files:
     # Definir los argumentos necesarios para la función main
     response = None  # Puedes proporcionar el valor adecuado si es necesario
-    
+
+    # Obtener el nombre base del archivo PDF (sin la extensión)
+    pdf_basename = os.path.basename(pdf_file)
+    pdf_name_without_extension = os.path.splitext(pdf_basename)[0]
+
+    # Nombre del archivo de Excel con la misma base que el archivo PDF
+    excel_filename = f"{pdf_name_without_extension}.xlsx"
+
     # Llamar a la función main para convertir el PDF en Excel
-    result = main(response, pdf_file, reportType)
+    result = main(response, pdf_file, reportType, excel_filename)
 
     if result:
         print(f"La conversión del archivo {pdf_file} se realizó con éxito.")
