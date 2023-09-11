193db8f25d5516f877c7de790b6f90144a7e4bc1
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-09-11 16:14:43 -0600

    test 3

--- QA/convert_pdf_API.py
+++ QA/convert_pdf_API.py
@@ -16,7 +16,7 @@ pdf_files = ["QA/PDF/API/Dawson, Kathleen.pdf",
              #"QA/PDF/API/Delta Health 4.15.23.pdf", 
              #"QA/PDF/API/Hannibal 4.15.23.pdf", 
              "QA/PDF/API/Mattox, Kyle.pdf", 
-             #"QA/PDF/API/TMMC W.E. 4.22.pdf",
+             "QA/PDF/API/TMMC W.E. 4.22.pdf",
              #"QA/PDF/API/API Empty.pdf"
              ]  
 
