12c241a4e31056715e9fd28fe19e3b77251a93ef
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-09-11 16:18:18 -0600

    testTMMC

--- QA/convert_pdf_API.py
+++ QA/convert_pdf_API.py
@@ -12,10 +12,10 @@ sys.path.append(os.path.join(base_dir, ".."))  # ".." significa el directorio pa
 from Modules.pdfAPI import main
 
 # Lista de nombres de archivos PDF a convertir
-pdf_files = ["QA/PDF/API/Dawson, Kathleen.pdf", 
+pdf_files = [#"QA/PDF/API/Dawson, Kathleen.pdf", 
              #"QA/PDF/API/Delta Health 4.15.23.pdf", 
              #"QA/PDF/API/Hannibal 4.15.23.pdf", 
-             "QA/PDF/API/Mattox, Kyle.pdf", 
+             #"QA/PDF/API/Mattox, Kyle.pdf", 
              "QA/PDF/API/TMMC W.E. 4.22.pdf",
              #"QA/PDF/API/API Empty.pdf"
              ]  
