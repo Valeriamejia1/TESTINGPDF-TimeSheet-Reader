dbac629b7e5810669a66c8b6a01e87746902ea70
RodrigoFallas <RodrigoFallas@CR-RFALLAS>
2023-09-12 10:35:37 -0600

    AddKronosTC

--- QA/convert_pdf_Default.py
+++ QA/convert_pdf_Default.py
@@ -11,11 +11,11 @@ from Modules.pdfDefault import main
 
 # Lista de nombres de archivos PDF a convertir
 pdf_files = ["QA/PDF/Default/06-11.pdf",                                     #No se cae
-             "QA/PDF/Default/1690203601050_1994364726.pdf",
-             #"QA/PDF/Default/1690808400472_1671940182.pdf",
+             #"QA/PDF/Default/1690203601050_1994364726.pdf",                 Se cae jenkins
+             #"QA/PDF/Default/1690808400472_1671940182.pdf",                 Se cae Jenkins
              #"QA/PDF/Default/Aya- WE 8.28.22.pdf",                          Se cae Jenkins
              #"QA/PDF/Default/Combined File.pdf",                            Se cae jenkins
-             #"QA/PDF/Default/Kronos Timecards TC 07-30-22.pdf",
+             "QA/PDF/Default/Kronos Timecards TC 07-30-22.pdf",
              #"QA/PDF/Default/Scripps Approved Kronos we 6.25.22.pdf",
              #"QA/PDF/Default/Time Detail_100822-102122.pdf",                Se cae jenkins
              #"QA/PDF/Default/Time Detail_July152022.pdf",                   Se cae jenkins
